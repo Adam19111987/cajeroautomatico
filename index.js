56072b73c340ef62d7421fd2f730bdb0181aff75
AdamDev <a.diaz.1987.08@hotmail.com>
2024-08-16 15:16:32 -0400

    trabajo finalizado

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 precio = 400000
+nuevo_precio = Math.abs(precio)
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+cantidadSpan = document.querySelector('.cantidad');
+incrementar = document.querySelector('.incrementar');
+descremento = document.querySelector('.descremento');
+total_pagar = document.querySelector('.valor-total');
+
+incrementar.addEventListener('click', function(){
+    cantidad = Number(cantidadSpan.innerHTML);
+    nuevo_incremento = cantidad + 1;
+    cantidadSpan.innerHTML = nuevo_incremento;
+
+    valor = nuevo_precio * nuevo_incremento;
+    total_pagar.innerHTML = valor;
+
+
+})
+
+descremento.addEventListener('click', function(){
+ 
+    cantidad = Number(cantidadSpan.innerHTML);
+    nuevo_descremento = cantidad - 1;
+    if(nuevo_descremento >= 0)
+    {cantidadSpan.innerHTML = nuevo_descremento;}
+    else{}
+    
+    valor = nuevo_precio * nuevo_descremento;
+    total_pagar.innerHTML = valor;
+
+
+})
